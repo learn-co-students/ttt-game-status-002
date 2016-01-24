3893eb50a1dacc7ba12846f46b33e70e2c070a3a
alexv7 <alexvarjabedian7@gmail.com>
2016-01-24 11:31:05 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,49 +16,30 @@ WIN_COMBINATIONS = [
                      ]
 
 def won?(board)
-  
-#   win_index_1 = win_combination[0]
-#   win_index_2 = win_combination[1]
-#   win_index_3 = win_combination[2]
-#   win_index_4 = win_combination[3]
-#   win_index_5 = win_combination[4]
-#   win_index_6 = win_combination[5]
-#   win_index_7 = win_combination[6]
-#   win_index_8 = win_combination[7]
-#   win_index_9 = win_combination[8]
-
-
-i = 0 
-while i < WIN_COMBINATIONS.length
-  if (board[WIN_COMBINATIONS[i][0]] == board[WIN_COMBINATIONS[i][1]]) && (board[WIN_COMBINATIONS[i][1]] == board[WIN_COMBINATIONS[i][2]]) && (board[WIN_COMBINATIONS[i][1]] != " ")
-    return WIN_COMBINATIONS[i]
-  else
-    false
+  i = 0 
+  while i < WIN_COMBINATIONS.length
+    if (board[WIN_COMBINATIONS[i][0]] == board[WIN_COMBINATIONS[i][1]]) && (board[WIN_COMBINATIONS[i][1]] == board[WIN_COMBINATIONS[i][2]]) && (board[WIN_COMBINATIONS[i][1]] != " ")
+      return WIN_COMBINATIONS[i]
+    else
+      false
+    end
+    i += 1
   end
-  i += 1
-end
-
-
 end
 
 def full?(board)
-    i = 0
-    while i < board.length
-      if board[i] == " "
-        return false
-      else
-        return true
-      end
-      i += 1
-    end
+    return true if !board.include? " "
+    false
 end
 
 def draw?(board)
-  if full?(board) == true && won?(board) == false
-    return true
-  else
+  if won?(board).is_a?(Array)
     return false
   end
+  if full?(board) == true 
+    return true
+  end
+  false
 end
 
 def over?(board)
@@ -70,9 +51,8 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) != false
-    board[won?(board)[0]]
-  end
+  return board[won?(board)[0]] if won?(board)
+  nil
 end
 
 
