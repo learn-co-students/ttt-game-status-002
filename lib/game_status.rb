eb05ff0b2d6fd4b7c2079c01be4816217a42ee55
beingy <beingy@gmail.com>
2016-01-28 22:00:50 +0000

    nitrous-backup at Thu Jan 28 22:00:50 UTC 2016

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,23 +3,35 @@ def position_taken?(board, location)
   !(board[location].nil? || board[location] == " ")
 end
 
-def won?(board)
- if board == [" "," "," "," "," "," "," "," "," "]
-   return false
- else
-  WIN_COMBINATIONS.detect { |winners|
-    (board[winners[0]] == "X" && board[winners[1]] == "X" && board[winners[2]] == "X") ||
-    (board[winners[0]] == "O" && board[winners[1]] == "O" && board[winners[2]] == "O") }
- end
-end
+#def won?(board)
+#if board == [" "," "," "," "," "," "," "," "," "]
+#   return false
+# else
+#  WIN_COMBINATIONS.detect { |winners|
+#    (board[winners[0]] == "X" && board[winners[1]] == "X" && board[winners[2]] == "X") ||
+#    (board[winners[0]] == "O" && board[winners[1]] == "O" && board[winners[2]] == "O") }
+#   winning_combo = [board[winners[0]],board[winners[1]], board[winners[2]]]
+#   puts winning_combo
+# end
+#end
 
 #def won?(board)
 #  WIN_COMBINATIONS.detect do |combo|
-#    board[combo[0]] == board[combo[1]] &&
+#    (board[combo[0]] == board[combo[1]] &&
 #    board[combo[1]] == board[combo[2]] &&
-#    position_taken?(board, combo[0])
+#    position_taken?(board, combo[0]))
 #  end
 #end
+
+def won?(board)
+  WIN_COMBINATIONS.select do |combo|
+    if board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
+      winning_combo = [combo[0],combo[1], board[combo[2]]]
+      puts winning_combo
+      puts WIN_COMBINATIONS
+    end
+  end
+end
 
 def full?(board)
   board.all? { |location| location != " "}
@@ -83,5 +95,5 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
 #[0,4,8], #left diagonal
 #[6,4,2]] #right diagonal
 
-#board = ["X","X","X","O","O"," "," "," "," "]
-#winner(board)
+board = ["X","X","X","O","O"," "," "," "," "]
+won?(board)
